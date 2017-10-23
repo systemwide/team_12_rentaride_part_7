213ce0c8f8c1278bf86609ac581def6f64f04b87
benrotolo <benrotolo@192.168.0.103>
2017-10-23 13:41:34 -0400

    HourlyPriceImpl.java

--- rentaride/src/edu/uga/cs/rentaride/entity/impl/HourlyPriceImpl.java
+++ rentaride/src/edu/uga/cs/rentaride/entity/impl/HourlyPriceImpl.java
@@ -29,55 +29,55 @@ public class HourlyPriceImpl
 	
 	@Override
 	public long getId() {
-		// TODO Auto-generated method stub
+
 		return id;
 	}
 
 	@Override
 	public void setId(long id) {
-		// TODO Auto-generated method stub
+
 		this.id = id;
 	}
 
 	@Override
 	public boolean isPersistent() {
-		// TODO Auto-generated method stub
+
 		return id == -1;
 	}
 
 	@Override
 	public int getMaxHours() {
-		// TODO Auto-generated method stub
+
 		return maxHrs;
 	}
 
 	@Override
 	public void setMaxHours(int maxHours) throws RARException {
-		// TODO Auto-generated method stub
+
 		this.maxHrs = maxHours;
 	}
 
 	@Override
 	public int getPrice() {
-		// TODO Auto-generated method stub
+
 		return price;
 	}
 
 	@Override
 	public void setPrice(int price) throws RARException {
-		// TODO Auto-generated method stub
+
 		this.price = price;
 	}
 
 	@Override
 	public VehicleType getVehicleType() {
-		// TODO Auto-generated method stub
+
 		return vehicleType;
 	}
 
 	@Override
 	public void setVehicleType(VehicleType vehicleType) throws RARException {
-		// TODO Auto-generated method stub
+
 		this.vehicleType = vehicleType;
 	}
 	
