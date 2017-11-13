86c3407d35e30ba25a19213c0d9aaa6215ee8498
benrotolo <benrotolo@192.168.0.101>
2017-11-12 20:41:42 -0500

    changes to presentation layer, some html files

--- rentaride/src/edu/uga/cs/rentaride/presentation/login_servlet.java
+++ rentaride/src/edu/uga/cs/rentaride/presentation/login_servlet.java
@@ -17,83 +17,104 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class admin_login_servlet
  */
 @WebServlet("/login_servlet")
-public class login_servlet extends HttpServlet {
+public class login_servlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public login_servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    private boolean isValid(String email, String password, Connection con, HttpServletRequest request, HttpServletResponse response, String admin)
-    {
-    	String dbEmail = "";
-    	if(admin != null) //use Admin table
-    	{
-    		String query = "SELECT email FROM Administrator WHERE password=\""+password+"\"";
-    		ResultSet rs = DatabaseAccess.retrieve(con, query);
-    		try
-    		{
-    			while (rs.next())
-    			{
-    				dbEmail = rs.getString("email");
-    			}
-    		} catch (SQLException e)
-    		{
-    			e.printStackTrace();
-    		}
-    	}
-    	else //use customer table
-    	{
-    		//check email and password against the database
-    	String query = "SELECT email FROM Customer WHERE password=\""+password+"\"";
-		ResultSet rs = DatabaseAccess.retrieve(con, query);
-		try
-		{
-			while (rs.next())
-			{
-				dbEmail = rs.getString("email");
-			}
-		} catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		/*
-		String dbPass = "";
-		String query2 = "SELECT password FROM Customer WHERE password=\""+password+"\"";
-		ResultSet rs2 = DatabaseAccess.retrieve(con, query2);
-		try
-		{
-			while (rs.next())
-			{
-				dbPass = rs2.getString("password");
-			}
-		} catch (SQLException e)
-		{
-			e.printStackTrace();
-		}*/
-    	}
-    	System.out.println("Email: " + email);
-		System.out.println("Password: " + password);
-		System.out.println("DBEmail: " + dbEmail);
-		//System.out.println("DBPass: " + dbPass);
-		if(email.equalsIgnoreCase(dbEmail))
-		{
-			return true;
-		}
-		else
-		{
-    			return false;
-		}
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public login_servlet()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private boolean isValid(String email, String password, Connection con, HttpServletRequest request,
+			HttpServletResponse response, String admin)
+	{
+		String dbEmail = "";
+		String dbPass = "";
+		if (admin != null) // use Admin table
+		{
+			String query = "SELECT email FROM Administrator WHERE password=\"" + password + "\"";
+			ResultSet rs = DatabaseAccess.retrieve(con, query);
+			try
+			{
+				while (rs.next())
+				{
+					dbEmail = rs.getString("email");
+				}
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+			String query2 = "SELECT password FROM Administrator WHERE password=\"" + password + "\"";
+			ResultSet rs2 = DatabaseAccess.retrieve(con, query2);
+			try
+			{
+				while (rs2.next())
+				{
+					dbPass = rs2.getString("password");
+				}
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+		} else // use customer table
+		{
+			// check email and password against the database
+			String query = "SELECT email FROM Customer WHERE password=\"" + password + "\"";
+			ResultSet rs = DatabaseAccess.retrieve(con, query);
+			try
+			{
+				while (rs.next())
+				{
+					dbEmail = rs.getString("email");
+				}
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+			String query2 = "SELECT password FROM Customer WHERE password=\"" + password + "\"";
+			ResultSet rs2 = DatabaseAccess.retrieve(con, query2);
+			try
+			{
+				while (rs2.next())
+				{
+					dbPass = rs2.getString("password");
+				}
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		/*//For Testing
+			System.out.println("Email: " + email);
+			System.out.println("Password: " + password);
+			System.out.println("DBEmail: " + dbEmail);
+			System.out.println("DBPass: " + dbPass);
+			System.out.println();
+			*/
+		
+		if (email.equalsIgnoreCase(dbEmail) && password.equalsIgnoreCase(dbPass)) //email and password match the DB
+		{
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		response.setContentType("text/html");
 		System.out.println("Connecting to database...");
 		Connection con = DatabaseAccess.connect();
@@ -106,39 +127,37 @@ public class login_servlet extends HttpServlet {
 		String email = request.getParameter("email");
 		String pass = request.getParameter("password");
 		String admin = request.getParameter("admin");
-		
+
 		/*
-		 * 4 possible outcomes:
-		 * isAdmin, isValid
-		 * isAdmin, invalid credentials
-		 * isCustomer, isValid
-		 * isCustomer, invalid credentials
+		 * 4 possible outcomes: isAdmin, isValid isAdmin, invalid credentials
+		 * isCustomer, isValid isCustomer, invalid credentials
 		 */
-		
-		if(admin == null) //Customer
+
+		if (admin == null) // Customer
 		{
-			if(isValid(email, pass, con, request, response, admin)) //show user login home page
+			if (isValid(email, pass, con, request, response, admin)) // show user login home page
 			{
-				HttpSession session=request.getSession();  
-		        session.setAttribute("email",email); 
-				getServletConfig().getServletContext().getRequestDispatcher("/user_browse.html").forward(request, response);
-			}
-			else //show invalid login page; try again
+				HttpSession session = request.getSession();
+				session.setAttribute("email", email);
+				getServletConfig().getServletContext().getRequestDispatcher("/user_browse.html").forward(request,
+						response);
+			} else // show invalid login page; try again
 			{
-				getServletConfig().getServletContext().getRequestDispatcher("/invalid_login.html").forward(request, response);
+				getServletConfig().getServletContext().getRequestDispatcher("/invalid_login.html").forward(request,
+						response);
 			}
-		}
-		else //admin
+		} else // admin
 		{
-			if(isValid(email, pass, con, request, response, admin)) //show admin login home page
+			if (isValid(email, pass, con, request, response, admin)) // show admin login home page
 			{
-				HttpSession session=request.getSession();  
-		        session.setAttribute("email",email); 
-				getServletConfig().getServletContext().getRequestDispatcher("/admin_create_type.html").forward(request, response);
-			}
-			else //show invalid login page; try again
+				HttpSession session = request.getSession();
+				session.setAttribute("email", email);
+				getServletConfig().getServletContext().getRequestDispatcher("/admin_create_type.html").forward(request,
+						response);
+			} else // show invalid login page; try again
 			{
-				getServletConfig().getServletContext().getRequestDispatcher("/invalid_login.html").forward(request, response);
+				getServletConfig().getServletContext().getRequestDispatcher("/invalid_login.html").forward(request,
+						response);
 			}
 		}
 		out.println("</body></html>");
@@ -153,9 +172,11 @@ public class login_servlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		doGet(request, response);
 	}
 
--- rentaride/src/edu/uga/cs/rentaride/presentation/signup_servlet.java
+++ rentaride/src/edu/uga/cs/rentaride/presentation/signup_servlet.java
@@ -3,6 +3,9 @@ package edu.uga.cs.rentaride.presentation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,63 +18,112 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class signup_servlet
  */
 @WebServlet("/signup_servlet")
-public class signup_servlet extends HttpServlet {
+public class signup_servlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public signup_servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public signup_servlet()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private boolean registeredSuccessfully(Connection con, String firstname, String lastname, String username,
+			String email, String licenseNo, String licenseState, String pass, String passRepeat,
+			HttpServletRequest request, HttpServletResponse response)
+	{
+		// use logic layer methods to insert into database
+		// if no error return true;
+		// else return false;
+		String address = "543 Second Street, Athens, GA 30605";
+		String createDate = "0000-00-00 00:00:00";
+		String memberUntil = "0000-00-00";
+		String ccNumb = "placeholder";
+		String ccExp = "0000-00-00";
+		String status = "active";
+
+		String query = "INSERT INTO Customer (firstName, lastName, userName, email, password, licState, licNumber, address, createDate, memberUntil, ccNumb, ccExp, status) "
+				+ "VALUES ('" + firstname + "','" + lastname + "','" + username + "','" + email + "', '" + pass + "', '"
+				+ licenseState + "', '" + licenseNo + "','" + address + "', '" + createDate + "', '" + memberUntil + "', '"+ ccNumb + "', '" + ccExp + "', '"+ status +"');";
+		Statement stmt = null;
+
+		try
+		{
+			stmt = con.createStatement();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			stmt.executeUpdate(query);
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println("Inserted " + firstname + " Into Customer Table");
+		return true;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		response.setContentType("text/html");
-		//System.out.println("Connecting to database...");
-		//Connection con = DatabaseAccess.connect(); //change database access class to team12 database
-		//System.out.println("Connected to database successfully.\n");
-		//getServletConfig().getServletContext().getRequestDispatcher("/admin_login.html").forward(request, response);
+		System.out.println("Connecting to database...");
+		Connection con = DatabaseAccess.connect(); // change database access class to team12 database
+		System.out.println("Connected to database successfully.\n");
+		// getServletConfig().getServletContext().getRequestDispatcher("/admin_login.html").forward(request,
+		// response);
 		PrintWriter out = response.getWriter();
 		out.println("<html><head>");
 		out.println("<title>Signup</title>");
 		out.println("</head>");
 		out.println("<body>");
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String username = request.getParameter("username");
 		String email = request.getParameter("email");
 		String licenseNo = request.getParameter("licenseNo");
 		String licenseState = request.getParameter("licenseState");
 		String pass = request.getParameter("psw");
 		String passRepeat = request.getParameter("psw-repeat");
-		
-		if(!pass.equalsIgnoreCase(passRepeat)) //user did not correctly enter passwords 
+
+		if (!pass.equalsIgnoreCase(passRepeat)) // user did not correctly enter passwords
 		{
-			getServletConfig().getServletContext().getRequestDispatcher("/invalid_signup.html").forward(request, response);
+			getServletConfig().getServletContext().getRequestDispatcher("/invalid_signup.html").forward(request,
+					response);
 		}
-		
-		if(registeredSuccessfully(email, licenseNo, licenseState, pass, passRepeat))
+
+		if (registeredSuccessfully(con, firstname, lastname, username, email, licenseNo, licenseState, pass, passRepeat,
+				request, response))
 		{
-			HttpSession session=request.getSession();  
-			session.setAttribute("email",email); 
+			HttpSession session = request.getSession();
+			session.setAttribute("name", firstname);
 			getServletConfig().getServletContext().getRequestDispatcher("/user_browse.html").forward(request, response);
 		}
 		out.println("</body></html>");
-	}
-
-	private boolean registeredSuccessfully(String email, String licenseNo, String licenseState, String pass, String passRepeat)
-	{
-		//use logic layer methods to insert into database
-		//if no error return true;
-		//else return false;
-		return true; //change to false later
+		try
+		{
+			con.close();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
